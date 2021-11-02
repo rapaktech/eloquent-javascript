45b3110bcf822d1b53162f9e6187f77acc001bf3
Jim Ezesinachi <ezesinachijim@gmail.com>
2021-11-02 16:42:22 +0100

    work on Chapter 18 & removed '&' from folder names

---
+++ 18 - HTML and Forms/http.js
@@ -0,0 +1,4 @@
+console.log(encodeURIComponent("Yes?"));
+// → Yes%3F
+console.log(decodeURIComponent("Yes%3F"));
+// → Yes?
--- test.js
+++
@@ -1,5 +0,0 @@
-// For random tests of JavaScript functionality before implementing into code
-
-/\b\+?\d|-?\d|\+?\d+.(\d+)?|-?\d+.(\d+)?|\+?(\d+)?.\d+|\+?(\d+)?.\d+|\+?\d+e\d+|\-?\d+e\d+\b/i;
-
-/\b\+?|-?((\d.\d?|\d?.\d)|(\de\d+)|(\d+))\b/i;
