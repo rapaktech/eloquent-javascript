e636691e638d59840a1c276027862a3d794ff424
Jim Ezesinachi <ezesinachijim@gmail.com>
2022-01-29 12:05:54 +0100

    completed the book

--- 21 - Project - Skill-Sharing Website/router.js
+++ 21 - Project - Skill-Sharing Website/router.js
@@ -1,22 +1,21 @@
-const {parse} = require("url");
+var {parse} = require("url");
 
 module.exports = class Router {
-    constructor() {
-        this.routes = [];
-    }
+  constructor() {
+    this.routes = [];
+  }
+  add(method, url, handler) {
+    this.routes.push({method, url, handler});
+  }
+  resolve(context, request) {
+    let path = parse(request.url).pathname;
 
-    add(method, url, handler) {
-        this.routes.push({method, url, handler});
+    for (let {method, url, handler} of this.routes) {
+      let match = url.exec(path);
+      if (!match || request.method != method) continue;
+      let urlParts = match.slice(1).map(decodeURIComponent);
+      return handler(context, ...urlParts, request);
     }
-
-    resolve(context, request) {
-        let path = parse(request.url).pathname;
-        for (let { method, url, handler } of this.routes) {
-            let match = url.exec(path);
-            if (!match || request.method != method) continue;
-            let urlParts = match.slice(1).map(decodeURIComponent);
-            return handler(context, ...urlParts, request);
-        }
-        return null;
-    }
+    return null;
+  }
 };
